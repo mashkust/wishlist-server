0cfea75360f316eca83be62b3c3b5e7fa3a412c3
Мария Кустова <mashkusti@gmail.com>
2024-10-10 13:33:01 +0300

    correct cors

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+server.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://github.com/mashkust/mashkust.github.io"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(router);
 
 server.listen(port);
-
-
